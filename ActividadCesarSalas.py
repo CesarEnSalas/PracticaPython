1dbe6f2b5cd2be982ae50d64f4716ca9d6b05538
CesarSalas <cesarsalas110@gmail.com>
2024-10-16 12:36:17 +0200

    Se creo la clase Producto, junto con sus getters y setters

---
+++ ActividadCesarSalas.py
@@ -0,0 +1,33 @@
+class Producto:
+  def __init__(self, nombre, categoria, precio, cantidad):
+    self.__nombre = nombre
+    self.__categoria = categoria
+    self.set_precio(precio)
+    self.set_cantidad(cantidad)
+
+#Getters
+  def get_nombre(self):
+    return self.__nombre
+  
+  def get_categorio(self):
+    return self.__categoria
+  
+  def get_precio(self):
+    return self.__precio
+  
+  def get_cantidad(self):
+    return self.__cantidad
+  
+#Setters
+  def set_precio(self, precio):
+    if precio > 0:
+      self.__precio = precio
+    else:
+      raise ValueError("El precio debe ser mayor a 0")
+  
+  def set_cantidad(self, cantidad):
+    if cantidad > 0:
+      self.__cantidad = cantidad
+    else:
+      raise ValueError("La cantidad del producto tiene que ser mayor a 0")
+    
